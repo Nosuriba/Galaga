3d45bf77fa2f36188ff8187486803095a43735be
Nosuriba@11 <kitagawa_taiko@icloud.com>
2019-07-08 22:17:19 +0900

    いらないローカル変数を削除した

--- DebugDisp.cpp
+++ DebugDisp.cpp
@@ -104,7 +104,7 @@ void DebugDisp::SetWait()
 
 int DebugDisp::DrawBox(int x1, int y1, int x2, int y2, unsigned int Color, int FillFlag)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, _alpha);
@@ -113,38 +113,38 @@ int DebugDisp::DrawBox(int x1, int y1, int x2, int y2, unsigned int Color, int F
 								 Color, FillFlag);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return rtnFlag;
 }
 
 int DebugDisp::DrawGraph(int x, int y, int GrHandle, int TransFlag)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, _alpha);
 	int rtnFlag = DxLib::DrawGraph(x + LpGame.gameScreenPos.x, y + LpGame.gameScreenPos.y, GrHandle, TransFlag);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return rtnFlag;
 }
 
 int DebugDisp::DrawLine(int x1, int y1, int x2, int y2, unsigned int Color, int Thickness)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 	/// ‚Æ‚è‚ ‚¦‚¸“§‰ß‚È‚µ
 	int rtnFlag = DxLib::DrawLine(x1 + LpGame.gameScreenPos.x, y1 + LpGame.gameScreenPos.y,
 								  x2 + LpGame.gameScreenPos.x, y2 + LpGame.gameScreenPos.y,
 								  Color, Thickness);
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return rtnFlag;
 }
 
 int DebugDisp::DrawCircle(int x, int y, int r, unsigned int Color, int FillFlag, int LineThickness)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, _alpha);
@@ -152,31 +152,31 @@ int DebugDisp::DrawCircle(int x, int y, int r, unsigned int Color, int FillFlag,
 									Color, FillFlag, LineThickness);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return rtnFlag;
 }
 
 int DebugDisp::DrawPixel(int x, int y, unsigned int Color)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 
 	SetDrawBlendMode(DX_BLENDMODE_ALPHA, _alpha);
 	int rtnFlag = DxLib::DrawPixel(x + LpGame.gameScreenPos.x, y + LpGame.gameScreenPos.y, Color);
 	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
 
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return 0;
 }
 
 int DebugDisp::DrawString(int x, int y, const TCHAR * String, unsigned int Color, unsigned int EdgeColor)
 {
-	int _ghBefor = GetDrawScreen();
+	_beforScr = GetDrawScreen();
 	SetDrawScreen(_dbgScreen);
 
 	int rtnFlag = DxLib::DrawString(x, y, String, Color, EdgeColor);
 
-	SetDrawScreen(_ghBefor);
+	SetDrawScreen(_beforScr);
 	return rtnFlag;
 }
 #endif
